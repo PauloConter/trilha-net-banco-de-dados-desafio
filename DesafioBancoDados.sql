5a91b1651de5b593027e7fa02970a2e8cad3ea5d
Paulo Conter <conterdev@gmail.com>
2023-08-26 10:34:49 -0300

    Update DesafioBancoDados.sql

--- DesafioBancoDados.sql
+++ DesafioBancoDados.sql
@@ -1,4 +1,4 @@
----- 1 ---- 
+---- 1 ----  
 SELECT
 	Nome,
 	Ano
@@ -81,7 +81,7 @@ SELECT
 FROM Filmes f
 INNER JOIN FilmesGenero fg ON f.Id = fg.IdFilme
 INNER JOIN Generos g ON fg.IdGenero = g.Id
-WHERE g.Genero = 'Mistério'
+WHERE g.Genero = 'MistÃ©rio'
 
 ----- 12 ------ 
 SELECT
